4c6fc02d7ac1efeb211b3e9affb2141b42c6473f
jokim21 <jokim21@amherst.edu>
2021-03-08 17:01:46 -0500

    cleaned up problem b

--- Homework/ps3a_shiny/app.r
+++ Homework/ps3a_shiny/app.r
@@ -1,20 +1,7 @@
 library(shiny)
 
 ui <- fluidPage(
-      navlistPanel(              
-    tabPanel(title = "Normal data",
-             plotOutput("norm"),
-             actionButton("renorm", "Resample")
-    ),
-    tabPanel(title = "Uniform data",
-             plotOutput("unif"),
-             actionButton("reunif", "Resample")
-    ),
-    tabPanel(title = "Chi Squared data",
-             plotOutput("chisq"),
-             actionButton("rechisq", "Resample"))
-    ),
-    sliderInput(inputId = "num", 
+  sliderInput(inputId = "num", 
               label = "Choose a number", 
               value = 25, min = 1, max = 100),
   textInput(inputId = "title", 
@@ -22,18 +9,35 @@ ui <- fluidPage(
             value = "Histogram of Random Normal Values"),
   actionButton(inputId = "update", 
                label = "Update"),
+  navlistPanel(              
+    tabPanel(title = "Normal data",
+             plotOutput("norm"),
+    ),
+    tabPanel(title = "Uniform data",
+             plotOutput("unif"),
+    ),
+    tabPanel(title = "Chi Squared data",
+             plotOutput("chisq"),
+    ),
   plotOutput("hist"),
-  verbatimTextOutput("stats")
+  verbatimTextOutput("stats"))
 )
+
 
 server <- function(input, output) {
   data <- eventReactive(input$update, {
     rnorm(input$num)
   })
+  
   rv <- reactiveValues(
-    norm = rnorm(500), 
-    unif = runif(500),
-    chisq = rchisq(500, 2))
+    norm = rnorm(100), 
+    unif = runif(100),
+    chisq = rchisq(100, 2))
+  
+  observeEvent(input$renorm, { rv$norm <- rnorm(100) })
+  observeEvent(input$reunif, { rv$unif <- runif(100) })
+  observeEvent(input$rechisq, { rv$chisq <- rchisq(100, 2) })
+  
   observeEvent(input$update, {
     print(input$title)
   })
@@ -47,16 +51,16 @@ server <- function(input, output) {
   output$title <- renderText({
     input$title})
   output$norm <- renderPlot({
-    hist(rv$norm, breaks = 30, col = "grey", border = "white",
-         main = "500 random draws from a standard normal distribution")
+    hist(data(),
+         main = isolate(input$title))
   })
   output$unif <- renderPlot({
-    hist(rv$unif, breaks = 30, col = "grey", border = "white",
-         main = "500 random draws from a standard uniform distribution")
+    hist(rv$unif,
+        main = "100 random draws from a standard uniform distribution")
   })
   output$chisq <- renderPlot({
-    hist(rv$chisq, breaks = 30, col = "grey", border = "white",
-         main = "500 random draws from a Chi Square distribution with two degree of freedom")
+    hist(rv$chisq,
+         main = "100 random draws from a random draws from a Chi Square distribution with two degree of freedom")
   })
 }
 
